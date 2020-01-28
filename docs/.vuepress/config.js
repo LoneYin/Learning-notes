42ad901ef609ebcc4fd5cccf83df7dd472fb303a
尹龙云 <lone_yin@163.com>
2020-01-28 16:58:02 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -38,6 +38,10 @@ module.exports = {
             "advanced/函数柯里化",
             "advanced/bind和call和apply",
             "advanced/常见算法",
+            "advanced/单例模式",
+            "advanced/观察者模式",
+            "advanced/使用setTimeout模拟setInterval",
+            "advanced/new运算符",
           ]
         },
         {
@@ -47,13 +51,16 @@ module.exports = {
             "others/模块加载机制",
             "others/前端性能优化",
             "others/移动端适配方案",
-            "others/Vue与React的区别"
+            "others/Vue与React的区别",
+            "others/面试题整理"
           ]
         },
         {
           title: '个人随笔',
           collapsable: false,
           children: [
+            "essay/给ReactRouter添加转场动画",
+            "essay/Vue列表渲染中的key的作用",
             "essay/vue源码阅读知识点整理"
           ]
         }
@@ -61,7 +68,7 @@ module.exports = {
     },
     nav: [
       {
-        text: "笔记",
+        text: "前端笔记",
         link: "/notes/"
       }
     ]
